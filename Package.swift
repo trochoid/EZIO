4143818ce97497e77c95617889e5decf20b7a6a8
trochoid <trochoid@users.noreply.github.com>
2022-04-12 04:36:14 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "EZIO",
-    platforms: [ .iOS(.v13) ],
+    platforms: [ .iOS(.v14) ],
     products: [ .library(name: "EZIO", targets: ["EZIO"]) ],
     targets: [ .target(name: "EZIO", path: "") ]
 )
